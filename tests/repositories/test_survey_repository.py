6fa891319001d389e0948098d9b313aa2bda3917
Otto <otto@masterbranch.io>
2020-05-05 18:40:02 +0100

    covering missig functions

--- tests/repositories/test_survey_repository.py
+++ tests/repositories/test_survey_repository.py
@@ -20,3 +20,13 @@ class SurveyRepositoryTest(unittest.TestCase):
         empty_survey = {"id": uuid_input}
         uuid_str = sr.add_or_update(empty_survey)
         self.assertEqual(uuid_input, uuid_str)
+
+    def test_get_raises_ValueError_by_default(self):
+        survey_id = "004b0c5e-e581-4005-abc7-985f71935a6a"
+        sr = SurveyRepository()
+        self.assertRaises(ValueError, sr.get, survey_id)
+
+    def test_get_by_user_id_returns_empty_list_by_default(self):
+        sr = SurveyRepository()
+        surveys = sr.get_by_user_id(1)
+        self.assertEqual([], surveys)
